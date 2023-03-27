fcbc646a84958237b08696466d39f2384c960cc6
Matt Walden <mattwalden@gmail.com>
2023-03-27 11:46:36 +0100

    add GET /api/categories endpoint with associated testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,6 +10,41 @@ beforeEach(() => {
 
 afterAll(() => db.end());
 
-it("should ", () => {
-  expect(1).toBe(1);
+describe("GET /api/categories", () => {
+  it("200: Should respond with an array of category objects", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        const { categories } = body;
+        expect(categories).toBeInstanceOf(Array);
+        expect(categories).toHaveLength(4);
+        categories.forEach((category) => {
+          expect(category).toBeInstanceOf(Object);
+        });
+      });
+  });
+  it("200: response objects should have expected shape", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        const { categories } = body;
+        categories.forEach((category) => {
+          expect(category).toHaveProperty("slug", expect.any(String));
+          expect(category).toHaveProperty("description", expect.any(String));
+        });
+      });
+  });
+});
+
+describe("Typo 404 Error Handling", () => {
+  it("404: reponds page not found when a misspelt path is given", () => {
+    return request(app)
+      .get("/aip/cass")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("404 Path Not Found");
+      });
+  });
 });
