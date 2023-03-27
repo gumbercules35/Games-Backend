aa7d77223449ad3038dc9011b5b26551e261e965
Matt Walden <mattwalden@gmail.com>
2023-03-27 11:15:10 +0100

    app.js and related test file setup

---
+++ __tests__/app.test.js
@@ -0,0 +1,15 @@
+const db = require(`${__dirname}/../db/connection.js`);
+const { app } = require(`${__dirname}/../src/app.js`);
+const data = require(`${__dirname}/../db/data/test-data/index.js`);
+const seed = require(`${__dirname}/../db/seeds/seed.js`);
+const request = require("supertest");
+
+beforeEach(() => {
+  return seed(data);
+});
+
+afterAll(() => db.end());
+
+it("should ", () => {
+  expect(1).toBe(1);
+});
