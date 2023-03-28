6959a93fc819267f6e65fe4b0d8e0faa8c76220a
Matt Walden <mattwalden@gmail.com>
2023-03-28 15:41:34 +0100

    removed commented code

--- src/models/fetchReviews.model.js
+++ src/models/fetchReviews.model.js
@@ -32,10 +32,3 @@ exports.fetchReviews = (category, sort_by = "created_at", order = "DESC") => {
       ORDER BY reviews.${sort_by} ${order}; `;
   return db.query(queryStr, queryParams).then(({ rows }) => rows);
 };
-
-// `SELECT reviews.owner, reviews.title, reviews.review_id, reviews.category, reviews.review_img_url, reviews.created_at, reviews.votes, reviews.designer, COUNT(comments.review_id) AS comment_count
-//       // FROM reviews
-//       // LEFT JOIN comments
-//       // ON reviews.review_id = comments.review_id
-//       // GROUP BY reviews.review_id
-//       // ORDER BY reviews.created_at DESC;`;
