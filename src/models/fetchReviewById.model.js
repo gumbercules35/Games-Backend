f78e756032161479aa6b115e934a36af02677bdf
Matt Walden <mattwalden@gmail.com>
2023-03-28 16:12:50 +0100

    add GET /api/reviews/:review_id comment_count key on returned review updated testing to include this new key

--- src/models/fetchReviewById.model.js
+++ src/models/fetchReviewById.model.js
@@ -3,9 +3,7 @@ const db = require(`${__dirname}/../../db/connection.js`);
 exports.fetchReviewById = (review_id) => {
   return db
     .query(
-      `SELECT * 
-  FROM reviews 
-  WHERE review_id = $1`,
+      `SELECT reviews.*, COUNT(comments.review_id) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id WHERE reviews.review_id = $1 GROUP BY reviews.review_id ORDER BY reviews.created_at DESC;`,
       [review_id]
     )
     .then(({ rows }) => {
