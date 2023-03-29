eb7276e9b74d1b3c3a4b5a9333f2771713ea9266
Matt Walden <mattwalden@gmail.com>
2023-03-29 10:06:19 +0100

    add Setup for Hosting API

---
+++ listen.js
@@ -0,0 +1,8 @@
+const { app } = require(`${__dirname}/src/app.js`);
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else console.log(`Listening on ${PORT}`);
+});
