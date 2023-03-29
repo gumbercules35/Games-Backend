bce2db22e0713501d19f3bcf61e6bb85b0044969
Matt Walden <mattwalden@gmail.com>
2023-03-29 10:19:23 +0100

    added start script

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,6 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
-const config =
-  ENV === "production"
-    ? { connectionString: process.env.DATABASE_URL, max: 2 }
-    : {};
-
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
@@ -13,5 +8,10 @@ require("dotenv").config({
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE not set");
 }
+
+const config =
+  ENV === "production"
+    ? { connectionString: process.env.DATABASE_URL, max: 2 }
+    : {};
 
 module.exports = new Pool(config);
--- listen.js
+++ listen.js
@@ -2,7 +2,5 @@ const { app } = require(`${__dirname}/src/app.js`);
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, (err) => {
-  if (err) {
-    console.log(err);
-  } else console.log(`Listening on ${PORT}`);
+  console.log(`Listening on ${PORT}`);
 });
